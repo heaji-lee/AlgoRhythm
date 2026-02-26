7aa8bf76a2af324c8a5a5d800e4f805240377775
Heaji Lee <helenheajilee@gmail.com>
2026-02-26 13:26:04 +0000

    completed MergeTwoSortedLists

--- AlgoRhythm.Problems/Easy/006_MergeTwoSortedLists.cs
+++ AlgoRhythm.Problems/Easy/006_MergeTwoSortedLists.cs
@@ -11,16 +11,26 @@ public class MergeTwoSortedLists {
     }
 
     public ListNode Solve(ListNode list1, ListNode list2) {
-        // Input: list1 = [1,2,4], list2 = [1,3,4]
-        // Output: [1,1,2,3,4,4]
-        // Example 2:
+        if (list1 == null) return list2;
 
-        // Input: list1 = [], list2 = []
-        // Output: []
-        // Example 3:
+        if (list2 == null) return list1;
 
-        // Input: list1 = [], list2 = [0]
-        // Output: [0]
-        
+        var newListNode = new ListNode();
+        var current = newListNode;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;     // attach the current node that list1 is pointing to 
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+
+            current = current.next;
+        }
+        current.next = list1 ?? list2;  // Whatever is left, attach it to the next one
+
+        return newListNode.next;
     }
 }
